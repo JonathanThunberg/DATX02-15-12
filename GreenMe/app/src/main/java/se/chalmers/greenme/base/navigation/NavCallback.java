00082fd36b3602350927795886ddcee8b63139b3
Fredro <fredro@fredro.org>
2015-03-24 08:58:15 +0100

    first

---
+++ GreenMe/app/src/main/java/se/chalmers/greenme/base/navigation/NavCallback.java
@@ -0,0 +1,8 @@
+package se.chalmers.greenme.base.navigation;
+
+/**
+ * Created by Fredrik on 2015-03-23.
+ */
+public interface NavCallback {
+    void onNavigationDrawerItemSelected(int position);
+}
---
+++ GreenMe/app/src/main/java/se/chalmers/greenme/base/navigation/NavItem.java
@@ -0,0 +1,32 @@
+package se.chalmers.greenme.base.navigation;
+
+import android.graphics.drawable.Drawable;
+
+/**
+ * Created by Fredrik on 2015-03-23.
+ */
+public class NavItem {
+    private String mText;
+    private Drawable mDrawable;
+
+    public NavItem(String text, Drawable drawable) {
+        mText = text;
+        mDrawable = drawable;
+    }
+
+    public String getText() {
+        return mText;
+    }
+
+    public void setText(String text) {
+        mText = text;
+    }
+
+    public Drawable getDrawable() {
+        return mDrawable;
+    }
+
+    public void setDrawable(Drawable drawable) {
+        mDrawable = drawable;
+    }
+}
