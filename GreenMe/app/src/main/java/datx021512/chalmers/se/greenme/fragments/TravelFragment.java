04d56b8fd3e80664e090bc77548a83d70a7b936f
materne <hanna.materne@live.se>
2015-04-23 08:44:12 +0200

    Start/Stop button to track user movement. Also tracking the total distance between start and stop.

--- GreenMe/app/src/main/java/datx021512/chalmers/se/greenme/fragments/TravelFragment.java
+++ GreenMe/app/src/main/java/datx021512/chalmers/se/greenme/fragments/TravelFragment.java
@@ -3,17 +3,24 @@ package datx021512.chalmers.se.greenme.fragments;
 import android.app.Activity;
 import android.app.Fragment;
 
+import android.content.Context;
+import android.graphics.Color;
 import android.location.Location;
 
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.location.LocationProvider;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -21,20 +28,31 @@ import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+import junit.framework.TestCase;
+
+import java.util.List;
 
 import datx021512.chalmers.se.greenme.R;
-import datx021512.chalmers.se.greenme.navigation.NavAdapter;
 
 public class TravelFragment extends Fragment implements OnMapReadyCallback{
 
     private static final String TAG = "test";
-    private double startLatitude;
-    private double startLongitude;
-    private double endLatitude;
-    private double endLongitude;
 
     private MapView mapView;
     private GoogleMap map;
+    private boolean isButtonPressed = false;
+    private  Button mapButton;
+    private TextView textView;
+
+    private LocationManager locationManager = null;
+    private Location previousLocation = null;
+    private double totalDistance;
+
+    private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 250; // 100 meters
+    private static final long MIN_TIME_BW_UPDATES = 1000 * 60 * 1; // 1 minutes
 
     @Override
     public void onAttach(Activity activity){
@@ -72,19 +90,61 @@ public class TravelFragment extends Fragment implements OnMapReadyCallback{
         MapsInitializer.initialize(getActivity());
         map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
 
-
         CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(new LatLng(57.708870,11.974560),10);
         map.animateCamera(cameraUpdate);
 
+        mapButton=(Button)rootView.findViewById(R.id.mapbutton);
+        mapButton.setText("START");
+
+        textView=(TextView)rootView.findViewById(R.id.textView);
+
+        mapButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v)
+            {
+                // Enable or disable gps
+                if (!isButtonPressed) {
+                    startTracking();
+                }else{
+                    stopTracking();
+                    mapButton.setText("START");
+                    textView.setText("totdist" + totalDistance);
+
+
+                }
+
+                // Change button state
+                isButtonPressed = !isButtonPressed;
+            }
+        });
+
         return rootView;
+    }
+
+    //run method
+    public void startTracking() {
+        mapButton.setText("STOP");
+
+        // Get the location manager
+        locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
+
+        // Add new listeners with the given params
+        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, MIN_TIME_BW_UPDATES, MIN_DISTANCE_CHANGE_FOR_UPDATES, locationListener); // Network location
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, MIN_TIME_BW_UPDATES, MIN_DISTANCE_CHANGE_FOR_UPDATES, locationListener); // Gps location
 
 
     }
+    //stop method
+    public void stopTracking(){
+        locationManager.removeUpdates(locationListener);
+
+    }
+
+
     @Override
     public void onResume() {
         super.onResume();
         mapView.onResume();
-
     }
 
     @Override
@@ -103,22 +163,36 @@ public class TravelFragment extends Fragment implements OnMapReadyCallback{
     public void onMapReady(GoogleMap googleMap) {
 
     }
+    private LocationListener locationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location newLocation)
+        {
+            if (previousLocation != null)
+            {
+                double latitude = newLocation.getLatitude() + previousLocation.getLatitude();
+                latitude *= latitude;
+                double longitude = newLocation.getLongitude() + previousLocation.getLongitude();
+                longitude *= longitude;
+                double altitude = newLocation.getAltitude() + previousLocation.getAltitude();
+                altitude *= altitude;
+                totalDistance += Math.sqrt(latitude + longitude + altitude);
+            }
 
-    /**
-     * This method will return a straight line between the locations
-     * @return distance in kilometers
-     */
-    public double getDistanceInKiloMeters(){
-        Location loc1 = new Location("");
-        loc1.setLatitude(startLatitude);
-        loc1.setLongitude(startLongitude);
+            previousLocation = newLocation;
 
-        Location loc2 = new Location("");
-        loc2.setLatitude(endLatitude);
-        loc2.setLongitude(endLongitude);
+            Log.d(TAG, "totaldistance: "+ totalDistance);
 
-        float distanceInMeters = loc1.distanceTo(loc2);
-        float distanceInKiloMeters = distanceInMeters * 1000;
-        return distanceInKiloMeters;
-    }
+        }
+
+
+        @Override
+        public void onProviderDisabled(String provider) {}
+
+        @Override
+        public void onProviderEnabled(String provider) {}
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {}
+    };
+
 }
